0e57e25f22bc8a654f9e812942113aff304ed5a8
lentebloem <thisisreallyaname@gmail.com>
2016-04-09 12:45:38 -0700

    add dashboard

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
 
   // initialize the model
   $scope.user = 'GDPKYPW5A2PYCC2FPBCO7R2G5FHXMQBG6TX6IQYYYMXKCHXF2YNM3BOX';
-
+  $scope.accounts = [];
+  $scope.dataMatching = [];
+  $scope.msg = "[";
+  var hey = [];
   // $scope.udpateJson = function () {
     // helper for reformatting the Github API response into a form we can pass to D3
     var reformatAccountData = function (data) {
@@ -22,10 +25,24 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
       return [formattedData, formattedToken];
     };
 
-    var setGraphData = function(data) {
-      $scope.graphJson = data;
+    $scope.appendAccountEntry = function(key, entry){
+      $scope.dataMatching.push({
+          'name': key,
+          'size': 1,
+          'inputs': [entry]
+      });
+      hey.push({
+          'name': key,
+          'size': 1,
+          'inputs': [entry]
+      });
+      // console.log(hey);
+      $scope.msg = $scope.msg + JSON.stringify({
+          'name': key,
+          'size': 1,
+          'inputs': [entry]
+      }) + ',';
     }
-
     $scope.getCommitData = function () {
       $http({
         method: 'GET',
@@ -35,14 +52,7 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
         // attach this data to the scope
         $scope.data = reformatAccountData(data)[0];
         $scope.accounts = reformatAccountData(data)[1];
-        var dataMatching = $scope.dataMatching = [];
-        var appendAccountEntry = function(key, entry){
-          dataMatching.push({
-              "name": key,
-              "size": 1,
-              "inputs": [entry]
-          });
-        }
+
         var m;
         for (m=1; m<$scope.accounts.length; m+=1) {
           $http({
@@ -53,8 +63,7 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
               // attach this data to the scope
               var funder = data._embedded.records[0].funder;
               var key =  data._embedded.records[0].account;
-              appendAccountEntry(key, funder);
-              // console.log(dataMatching);
+              $scope.appendAccountEntry(key, funder);
             }).
             error(function (data, status) {
               if (status === 404) {
@@ -64,7 +73,6 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
               }
             });
           }
-          console.log(dataMatching);
           // clear the error messages
           $scope.error = '';
       }).
@@ -75,108 +83,110 @@ d3DemoApp.controller('AppCtrl', function AppCtrl ($scope, $http) {
           $scope.error = 'Error: ' + status;
         }
       });
+
+      var stateChange = function(newState) {
+          setTimeout(function () {
+            $scope.msg = $scope.msg + "]";
+            console.log($scope.msg);
+            var diameter = 960,
+                radius = diameter / 2,
+                innerRadius = radius - 120;
+
+            var cluster = d3.layout.cluster()
+                .size([360, innerRadius])
+                .sort(null)
+                .value(function(d) { return d.size; });
+
+            var bundle = d3.layout.bundle();
+
+            var line = d3.svg.line.radial()
+                .interpolate("bundle")
+                .tension(.85)
+                .radius(function(d) { return d.y; })
+                .angle(function(d) { return d.x / 180 * Math.PI; });
+
+            var svg = d3.select("body").append("svg")
+                .attr("width", diameter)
+                .attr("height", diameter)
+              .append("g")
+                .attr("transform", "translate(" + radius + "," + radius + ")");
+
+            d3.json("accountcreation.json", function(error, classes) {
+              if (error) throw error;
+
+              console.log(classes);
+              var nodes = cluster.nodes(packageHierarchy(classes)),
+                  links = packageImports(nodes);
+
+              svg.selectAll(".link")
+                  .data(bundle(links))
+                .enter().append("path")
+                  .attr("class", "link")
+                  .attr("d", line);
+
+              svg.selectAll(".node")
+                  .data(nodes.filter(function(n) { return !n.children; }))
+                .enter().append("g")
+                  .attr("class", "node")
+                  .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+                .append("text")
+                  .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+                  .attr("dy", ".31em")
+                  .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+                  .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+                  .text(function(d) { return d.key; });
+            });
+
+            d3.select(self.frameElement).style("height", diameter + "px");
+
+            // Lazily construct the package hierarchy from class names.
+            function packageHierarchy() {
+              var map = {};
+              function find(name, data) {
+                var node = map[name], i;
+                if (!node) {
+                  node = map[name] = data || {name: name, children: []};
+                  if (name.length) {
+                    node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+                    node.parent.children.push(node);
+                    node.key = name.substring(i + 1);
+                  }
+                }
+                return node;
+              }
+              angular.forEach(function(d) {
+                find(d.name, d);
+              });
+              return map[""];
+            }
+
+            // Return a list of imports for the given array of nodes.
+            function packageImports(nodes) {
+              var map = {},
+                  imports = [];
+              // Compute a map from name to node.
+              nodes.forEach(function(d) {
+                map[d.name] = d;
+              });
+              // For each import, construct a link from the source to target node.
+              nodes.forEach(function(d) {
+                if (d.imports) d.imports.forEach(function(i) {
+                  imports.push({source: map[d.name], target: map[i]});
+                });
+              });
+              return imports;
+            }
+
+          }, 5000);
+      };
+      stateChange();
     };
   // }();
 
   // get the commit data immediately
   $scope.getCommitData();
+  // console.log($scope.msg);
+  // $scope.$watch(function(){return $scope.msg;}, function(newValue, oldValue){console.log(newValue)});
 
-
-// ===========================
-// var renderD3  = function () {
-    var diameter = 960,
-        radius = diameter / 2,
-        innerRadius = radius - 120;
-
-    var cluster = d3.layout.cluster()
-        .size([360, innerRadius])
-        .sort(null)
-        .value(function(d) { return d.size; });
-
-    var bundle = d3.layout.bundle();
-
-    var line = d3.svg.line.radial()
-        .interpolate("bundle")
-        .tension(.85)
-        .radius(function(d) { return d.y; })
-        .angle(function(d) { return d.x / 180 * Math.PI; });
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", diameter)
-        .attr("height", diameter)
-      .append("g")
-        .attr("transform", "translate(" + radius + "," + radius + ")");
-
-    // $scope.$digest();
-    // $scope.$watch('', function (newVal) {
-      // ignore first call which happens before we even have data from the Github API
-      d3.json($scope.dataMatching, function(error, classes) {
-        if (error) throw error;
-
-        console.log(classes);
-        var nodes = cluster.nodes(packageHierarchy(classes)),
-            links = packageImports(nodes);
-
-        svg.selectAll(".link")
-            .data(bundle(links))
-          .enter().append("path")
-            .attr("class", "link")
-            .attr("d", line);
-
-        svg.selectAll(".node")
-            .data(nodes.filter(function(n) { return !n.children; }))
-          .enter().append("g")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-          .append("text")
-            .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
-            .attr("dy", ".31em")
-            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-            .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
-            .text(function(d) { return d.key; });
-      });
-
-      d3.select(self.frameElement).style("height", diameter + "px");
-
-      // Lazily construct the package hierarchy from class names.
-      function packageHierarchy() {
-        var map = {};
-        function find(name, data) {
-          var node = map[name], i;
-          if (!node) {
-            node = map[name] = data || {name: name, children: []};
-            if (name.length) {
-              node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
-              node.parent.children.push(node);
-              node.key = name.substring(i + 1);
-            }
-          }
-          return node;
-        }
-        angular.forEach(function(d) {
-          find(d.name, d);
-        });
-        return map[""];
-      }
-
-      // Return a list of imports for the given array of nodes.
-      function packageImports(nodes) {
-        var map = {},
-            imports = [];
-        // Compute a map from name to node.
-        nodes.forEach(function(d) {
-          map[d.name] = d;
-        });
-        // For each import, construct a link from the source to target node.
-        nodes.forEach(function(d) {
-          if (d.imports) d.imports.forEach(function(i) {
-            imports.push({source: map[d.name], target: map[i]});
-          });
-        });
-        return imports;
-      }
-    //
-    // });
-// =========================
 
 });
